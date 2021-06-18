24d163b6d5c32082221babd95e017d72b2eec1e4
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-18 10:29:02 +0900

    新規投稿機能を実装

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,22 +1,28 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.includes(:user)
   end
 
   def new
+    @food = Food.new
   end
 
   def create
+    food = current_user.foods.create!(food_params)
+    redirect_to food
   end
 
-  def show
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :comment)
   end
 end
