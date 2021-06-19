fb161fb85edf0f261ad9c871f8d28d3e8c24db34
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-19 09:14:31 +0900

    postsテーブルにimageカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_002002) do
+ActiveRecord::Schema.define(version: 2021_06_19_000838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_002002) do
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
