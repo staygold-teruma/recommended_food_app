ea95a8d9c88ee4fc98ebeb96aa5381a8fbf4a10f
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-19 10:00:32 +0900

    画像ファイル形式・ファイルサイズにバリデーションを設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,7 +36,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w[jpg gif png]
+    %w[jpg jpeg gif png]
+  end
+
+  def size_range
+    0..5.megabytes
   end
 
   # Override the filename of the uploaded files:
