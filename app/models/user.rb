da7b6dc9270e42e45007260f419473d9605b8e38
西川輝馬 <staygoldteruma@MacBookPro.local>
2021-06-23 11:21:42 +0900

    モデルを関連付けし、バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
   has_many :foods, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
